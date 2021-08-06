79641a4104f57e50a2d5a154e7b19930f4ee7f2d
Nicolas Correia <nicononoya@gmail.com>
2021-08-06 15:28:28 -0300

    Adding condition

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import pandas as pd     # Serve para importar um código do pandas
 lista_meses = ["1-Janeiro", "2-Fevereiro", "3-Março"]
 
 for mes in lista_meses:
-    print(mes)
     tabela_vendas = pd.read_excel(f"{mes}.xlsx")
-    print(tabela_vendas)
+    # 2 - Verificar se algum valor na coluna VENDAS daquele determinado arquivo é maior que 55.000
+    if (tabela_vendas["Vendas"] > 55000).any():
+        print(f"No mes de {mes} encontrou vendas acima de 55 mil")
 
-# 2 - Verificar se algum valor na coluna VENDAS daquele determinado arquivo é maior que 55.000
 # 3 - Caso seja maior que 55.000 -> Enviar uma SMS com o nome do ganhador e o mês em que foi feito, junto com as vendas
 # 4 - Caso não houver valor maior que 55.000, não realizar nenhuma operação
 # 5 - Bibliotecas a serem utilizadas para essa lógica:      pandas, openpyxl e twilio
