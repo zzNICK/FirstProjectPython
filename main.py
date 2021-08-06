8952a3b1fd870197e6a938920f9e95cd7312f1a0
Nicolas Correia <nicononoya@gmail.com>
2021-08-06 15:48:59 -0300

    Adding variables

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ for mes in lista_meses:
     tabela_vendas = pd.read_excel(f"{mes}.xlsx")
     # 2 - Verificar se algum valor na coluna VENDAS daquele determinado arquivo é maior que 55.000
     if (tabela_vendas["Vendas"] > 55000).any():
-        print(f"No mes de {mes} encontrou vendas acima de 55 mil")
+        vendedor = tabela_vendas.loc[tabela_vendas["Vendas"] > 55000 , "Vendedor"].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas["Vendas"] > 55000, "Vendas"]. values[0]
+        print(f"No mes de {mes} encontramos vendas acima de 55 mil. O Vendedor {vendedor} fez {vendas} vendas")
 
 # 3 - Caso seja maior que 55.000 -> Enviar uma SMS com o nome do ganhador e o mês em que foi feito, junto com as vendas
 # 4 - Caso não houver valor maior que 55.000, não realizar nenhuma operação
+
+
 # 5 - Bibliotecas a serem utilizadas para essa lógica:      pandas, openpyxl e twilio
